17283dacefb8c1ddf3bec720f59755772853bb78
yixiao1 <yi.xiao@e-campus.uab.cat>
2018-03-19 14:36:38 +0100

    update SSD loss and metrics

--- code/metrics/metrics.py
+++ code/metrics/metrics.py
@@ -298,3 +298,337 @@ def YOLOMetrics(input_shape=(3,640,640),num_classes=45,priors=[[0.25,0.25], [0.5
   else:
     return avg_recall
 
+
+"""
+    SSD loss function
+    code adapted from https://github.com/rykov8/ssd_keras
+"""
+def _l1_smooth_loss(y_true, y_pred):
+    """Compute L1-smooth loss.
+    # Arguments
+        y_true: Ground truth bounding boxes,
+            tensor of shape (?, num_boxes, 4).
+        y_pred: Predicted bounding boxes,
+            tensor of shape (?, num_boxes, 4).
+    # Returns
+        l1_loss: L1-smooth loss, tensor of shape (?, num_boxes).
+    # References
+        https://arxiv.org/abs/1504.08083
+    """
+    abs_loss = tf.abs(y_true - y_pred)
+    sq_loss = 0.5 * (y_true - y_pred)**2
+    l1_loss = tf.where(tf.less(abs_loss, 1.0), sq_loss, abs_loss - 0.5)
+    return tf.reduce_sum(l1_loss, -1)
+
+def _softmax_loss(y_true, y_pred):
+    """Compute softmax loss.
+    # Arguments
+        y_true: Ground truth targets,
+            tensor of shape (?, num_boxes, num_classes).
+        y_pred: Predicted logits,
+            tensor of shape (?, num_boxes, num_classes).
+    # Returns
+        softmax_loss: Softmax loss, tensor of shape (?, num_boxes).
+    """
+    y_pred = tf.maximum(tf.minimum(y_pred, 1 - 1e-15), 1e-15)
+    softmax_loss = -tf.reduce_sum(y_true * tf.log(y_pred),
+                                  axis=-1)
+    return softmax_loss
+
+def SSDLoss(input_shape=(3,640,640),num_classes=45,alpha=1,neg_pos_ratio=3,background_label_id=0,negatives_for_hard=100):
+  """ Arguments
+      num_classes: Number of classes including background.
+      alpha: Weight of L1-smooth loss.
+      neg_pos_ratio: Max ratio of negative to positive boxes in loss.
+      background_label_id: Id of background label.
+      negatives_for_hard: Number of negative boxes to consider
+          it there is no positive boxes in batch.
+  """
+  def _SSDLoss(y_true, y_pred, name=None):
+  
+    if background_label_id != 0:
+        raise Exception('Only 0 as background label id is supported')
+  
+    """Compute mutlibox loss.
+    # Arguments
+        y_true: Ground truth targets,
+            tensor of shape (?, num_boxes, 4 + num_classes + 8),
+            priors in ground truth are fictitious,
+            y_true[:, :, -8] has 1 if prior should be penalized
+                or in other words is assigned to some ground truth box,
+            y_true[:, :, -7:] are all 0.
+        y_pred: Predicted logits,
+            tensor of shape (?, num_boxes, 4 + num_classes + 8).
+    # Returns
+        loss: Loss for prediction, tensor of shape (?,).
+    """
+    batch_size = tf.shape(y_true)[0]
+    num_boxes = tf.to_float(tf.shape(y_true)[1])
+
+    # loss for all priors
+    conf_loss = _softmax_loss(y_true[:, :, 4:-8], y_pred[:, :, 4:-8])
+    loc_loss = _l1_smooth_loss(y_true[:, :, :4], y_pred[:, :, :4])
+
+    # get positives loss
+    num_pos = tf.reduce_sum(y_true[:, :, -8], axis=-1)
+    pos_loc_loss = tf.reduce_sum(loc_loss * y_true[:, :, -8], axis=1)
+    pos_conf_loss = tf.reduce_sum(conf_loss * y_true[:, :, -8], axis=1)
+
+    # get negatives loss, we penalize only confidence here
+    num_neg = tf.minimum(neg_pos_ratio * num_pos, num_boxes - num_pos)
+    pos_num_neg_mask = tf.greater(num_neg, 0)
+    has_min = tf.to_float(tf.reduce_any(pos_num_neg_mask))
+    num_neg = tf.concat(axis=0, values=[num_neg, [(1 - has_min) * negatives_for_hard]])
+    num_neg_batch = tf.reduce_min(tf.boolean_mask(num_neg, tf.greater(num_neg, 0)))
+    num_neg_batch = tf.to_int32(num_neg_batch)
+    confs_start = 4 + background_label_id + 1
+    confs_end = confs_start + num_classes - 1
+    max_confs = tf.reduce_max(y_pred[:, :, confs_start:confs_end], axis=2)
+    _, indices = tf.nn.top_k(max_confs * (1 - y_true[:, :, -8]), k=num_neg_batch)
+    batch_idx = tf.expand_dims(tf.range(0, batch_size), 1)
+    batch_idx = tf.tile(batch_idx, (1, num_neg_batch))
+    full_indices = (tf.reshape(batch_idx, [-1]) * tf.to_int32(num_boxes) + tf.reshape(indices, [-1]))
+    # full_indices = tf.concat(2, [tf.expand_dims(batch_idx, 2), tf.expand_dims(indices, 2)])
+    # neg_conf_loss = tf.gather_nd(conf_loss, full_indices)
+    neg_conf_loss = tf.gather(tf.reshape(conf_loss, [-1]), full_indices)
+    neg_conf_loss = tf.reshape(neg_conf_loss, [batch_size, num_neg_batch])
+    neg_conf_loss = tf.reduce_sum(neg_conf_loss, axis=1)
+
+    # loss is sum of positives and negatives
+    total_loss = pos_conf_loss + neg_conf_loss
+    total_loss /= (num_pos + tf.to_float(num_neg_batch))
+    num_pos = tf.where(tf.not_equal(num_pos, 0), num_pos, tf.ones_like(num_pos))
+    total_loss += (pos_loc_loss) / num_pos
+    
+    return total_loss
+  
+  return _SSDLoss
+
+
+"""
+    SSD detection metrics
+    code adapted from https://github.com/rykov8/ssd_keras
+"""
+def SSDMetrics(input_shape=(3,640,640),num_classes=45,alpha=1,neg_pos_ratio=3,background_label_id=0,negatives_for_hard=3):
+
+  def _SSDMetrics(y_true, y_pred, name=None):
+    """Utility class to do some stuff with bounding boxes and priors.
+    # Arguments
+        num_classes: Number of classes including background.
+        priors: Priors and variances, numpy tensor of shape (num_priors, 8),
+            priors[i] = [xmin, ymin, xmax, ymax, varxc, varyc, varw, varh].
+        overlap_threshold: Threshold to assign box to a prior.
+        nms_thresh: Nms threshold.
+        top_k: Number of total bboxes to be kept per image after nms step.
+    # References
+        https://arxiv.org/abs/1512.02325
+    """
+    # TODO add setter methods for nms_thresh and top_K
+    def __init__(self, num_classes, priors=None, overlap_threshold=0.5,
+                 nms_thresh=0.45, top_k=400):
+        self.num_classes = num_classes
+        self.priors = priors
+        self.num_priors = 0 if priors is None else len(priors)
+        self.overlap_threshold = overlap_threshold
+        self._nms_thresh = nms_thresh
+        self._top_k = top_k
+        self.boxes = tf.placeholder(dtype='float32', shape=(None, 4))
+        self.scores = tf.placeholder(dtype='float32', shape=(None,))
+        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
+                                                self._top_k,
+                                                iou_threshold=self._nms_thresh)
+        self.sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
+
+    @property
+    def nms_thresh(self):
+        return self._nms_thresh
+
+    @nms_thresh.setter
+    def nms_thresh(self, value):
+        self._nms_thresh = value
+        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
+                                                self._top_k,
+                                                iou_threshold=self._nms_thresh)
+
+    @property
+    def top_k(self):
+        return self._top_k
+
+    @top_k.setter
+    def top_k(self, value):
+        self._top_k = value
+        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
+                                                self._top_k,
+                                                iou_threshold=self._nms_thresh)
+
+    def iou(self, box):
+        """Compute intersection over union for the box with all priors.
+        # Arguments
+            box: Box, numpy tensor of shape (4,).
+        # Return
+            iou: Intersection over union,
+                numpy tensor of shape (num_priors).
+        """
+        # compute intersection
+        inter_upleft = np.maximum(self.priors[:, :2], box[:2])
+        inter_botright = np.minimum(self.priors[:, 2:4], box[2:])
+        inter_wh = inter_botright - inter_upleft
+        inter_wh = np.maximum(inter_wh, 0)
+        inter = inter_wh[:, 0] * inter_wh[:, 1]
+        # compute union
+        area_pred = (box[2] - box[0]) * (box[3] - box[1])
+        area_gt = (self.priors[:, 2] - self.priors[:, 0])
+        area_gt *= (self.priors[:, 3] - self.priors[:, 1])
+        union = area_pred + area_gt - inter
+        # compute iou
+        iou = inter / union
+        return iou
+
+    def encode_box(self, box, return_iou=True):
+        """Encode box for training, do it only for assigned priors.
+        # Arguments
+            box: Box, numpy tensor of shape (4,).
+            return_iou: Whether to concat iou to encoded values.
+        # Return
+            encoded_box: Tensor with encoded box
+                numpy tensor of shape (num_priors, 4 + int(return_iou)).
+        """
+        iou = self.iou(box)
+        encoded_box = np.zeros((self.num_priors, 4 + return_iou))
+        assign_mask = iou > self.overlap_threshold
+        if not assign_mask.any():
+            assign_mask[iou.argmax()] = True
+        if return_iou:
+            encoded_box[:, -1][assign_mask] = iou[assign_mask]
+        assigned_priors = self.priors[assign_mask]
+        box_center = 0.5 * (box[:2] + box[2:])
+        box_wh = box[2:] - box[:2]
+        assigned_priors_center = 0.5 * (assigned_priors[:, :2] +
+                                        assigned_priors[:, 2:4])
+        assigned_priors_wh = (assigned_priors[:, 2:4] -
+                              assigned_priors[:, :2])
+        # we encode variance
+        encoded_box[:, :2][assign_mask] = box_center - assigned_priors_center
+        encoded_box[:, :2][assign_mask] /= assigned_priors_wh
+        encoded_box[:, :2][assign_mask] /= assigned_priors[:, -4:-2]
+        encoded_box[:, 2:4][assign_mask] = np.log(box_wh /
+                                                  assigned_priors_wh)
+        encoded_box[:, 2:4][assign_mask] /= assigned_priors[:, -2:]
+        return encoded_box.ravel()
+
+    def assign_boxes(self, boxes):
+        """Assign boxes to priors for training.
+        # Arguments
+            boxes: Box, numpy tensor of shape (num_boxes, 4 + num_classes),
+                num_classes without background.
+        # Return
+            assignment: Tensor with assigned boxes,
+                numpy tensor of shape (num_boxes, 4 + num_classes + 8),
+                priors in ground truth are fictitious,
+                assignment[:, -8] has 1 if prior should be penalized
+                    or in other words is assigned to some ground truth box,
+                assignment[:, -7:] are all 0. See loss for more details.
+        """
+        assignment = np.zeros((self.num_priors, 4 + self.num_classes + 8))
+        assignment[:, 4] = 1.0
+        if len(boxes) == 0:
+            return assignment
+        encoded_boxes = np.apply_along_axis(self.encode_box, 1, boxes[:, :4])
+        encoded_boxes = encoded_boxes.reshape(-1, self.num_priors, 5)
+        best_iou = encoded_boxes[:, :, -1].max(axis=0)
+        best_iou_idx = encoded_boxes[:, :, -1].argmax(axis=0)
+        best_iou_mask = best_iou > 0
+        best_iou_idx = best_iou_idx[best_iou_mask]
+        assign_num = len(best_iou_idx)
+        encoded_boxes = encoded_boxes[:, best_iou_mask, :]
+        assignment[:, :4][best_iou_mask] = encoded_boxes[best_iou_idx,
+                                                         np.arange(assign_num),
+                                                         :4]
+        assignment[:, 4][best_iou_mask] = 0
+        assignment[:, 5:-8][best_iou_mask] = boxes[best_iou_idx, 4:]
+        assignment[:, -8][best_iou_mask] = 1
+        return assignment
+
+    def decode_boxes(self, mbox_loc, mbox_priorbox, variances):
+        """Convert bboxes from local predictions to shifted priors.
+        # Arguments
+            mbox_loc: Numpy array of predicted locations.
+            mbox_priorbox: Numpy array of prior boxes.
+            variances: Numpy array of variances.
+        # Return
+            decode_bbox: Shifted priors.
+        """
+        prior_width = mbox_priorbox[:, 2] - mbox_priorbox[:, 0]
+        prior_height = mbox_priorbox[:, 3] - mbox_priorbox[:, 1]
+        prior_center_x = 0.5 * (mbox_priorbox[:, 2] + mbox_priorbox[:, 0])
+        prior_center_y = 0.5 * (mbox_priorbox[:, 3] + mbox_priorbox[:, 1])
+        decode_bbox_center_x = mbox_loc[:, 0] * prior_width * variances[:, 0]
+        decode_bbox_center_x += prior_center_x
+        decode_bbox_center_y = mbox_loc[:, 1] * prior_width * variances[:, 1]
+        decode_bbox_center_y += prior_center_y
+        decode_bbox_width = np.exp(mbox_loc[:, 2] * variances[:, 2])
+        decode_bbox_width *= prior_width
+        decode_bbox_height = np.exp(mbox_loc[:, 3] * variances[:, 3])
+        decode_bbox_height *= prior_height
+        decode_bbox_xmin = decode_bbox_center_x - 0.5 * decode_bbox_width
+        decode_bbox_ymin = decode_bbox_center_y - 0.5 * decode_bbox_height
+        decode_bbox_xmax = decode_bbox_center_x + 0.5 * decode_bbox_width
+        decode_bbox_ymax = decode_bbox_center_y + 0.5 * decode_bbox_height
+        decode_bbox = np.concatenate((decode_bbox_xmin[:, None],
+                                      decode_bbox_ymin[:, None],
+                                      decode_bbox_xmax[:, None],
+                                      decode_bbox_ymax[:, None]), axis=-1)
+        decode_bbox = np.minimum(np.maximum(decode_bbox, 0.0), 1.0)
+        return decode_bbox
+
+    def detection_out(self, predictions, background_label_id=0, keep_top_k=200,
+                      confidence_threshold=0.01):
+        """Do non maximum suppression (nms) on prediction results.
+        # Arguments
+            predictions: Numpy array of predicted values.
+            num_classes: Number of classes for prediction.
+            background_label_id: Label of background class.
+            keep_top_k: Number of total bboxes to be kept per image
+                after nms step.
+            confidence_threshold: Only consider detections,
+                whose confidences are larger than a threshold.
+        # Return
+            results: List of predictions for every picture. Each prediction is:
+                [label, confidence, xmin, ymin, xmax, ymax]
+        """
+        mbox_loc = predictions[:, :, :4]
+        variances = predictions[:, :, -4:]
+        mbox_priorbox = predictions[:, :, -8:-4]
+        mbox_conf = predictions[:, :, 4:-8]
+        results = []
+        for i in range(len(mbox_loc)):
+            results.append([])
+            decode_bbox = self.decode_boxes(mbox_loc[i],
+                                            mbox_priorbox[i], variances[i])
+            for c in range(self.num_classes):
+                if c == background_label_id:
+                    continue
+                c_confs = mbox_conf[i, :, c]
+                c_confs_m = c_confs > confidence_threshold
+                if len(c_confs[c_confs_m]) > 0:
+                    boxes_to_process = decode_bbox[c_confs_m]
+                    confs_to_process = c_confs[c_confs_m]
+                    feed_dict = {self.boxes: boxes_to_process,
+                                 self.scores: confs_to_process}
+                    idx = self.sess.run(self.nms, feed_dict=feed_dict)
+                    good_boxes = boxes_to_process[idx]
+                    confs = confs_to_process[idx][:, None]
+                    labels = c * np.ones((len(idx), 1))
+                    c_pred = np.concatenate((labels, confs, good_boxes),
+                                            axis=1)
+                    results[-1].extend(c_pred)
+            if len(results[-1]) > 0:
+                results[-1] = np.array(results[-1])
+                argsort = np.argsort(results[-1][:, 1])[::-1]
+                results[-1] = results[-1][argsort]
+                results[-1] = results[-1][:keep_top_k]
+        return results
+  return _SSDMetrics
+
+
+
